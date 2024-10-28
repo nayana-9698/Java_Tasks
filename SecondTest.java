55b46b546d3f7dfb53b1c5d0797c96785db9cc0a
nayana <nayana9698@gmail.com>
2024-10-28 20:59:46 +0530

    adding java files

---
+++ SecondTest.java
@@ -0,0 +1,10 @@
+class SecondTest{
+public static void main(String args[])
+{
+int a =2;
+int b =3;
+
+System.out.println(a+b);
+}
+
+}
---
+++ ThirdTest.java
@@ -0,0 +1,6 @@
+class ThirdTest{
+public static void main(String args[])
+{
+System.out.println("We are executing third java program");
+}
+}
